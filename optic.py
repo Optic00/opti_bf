19a9aa105e1c5a17e26b70e7876a5960c32ce5f1
Optic00 <benjamin.busche@gmail.com>
2022-12-28 18:04:22 +0100

    Update optic.py

--- optic.py
+++ optic.py
@@ -39,3 +39,40 @@ last_number = read_last_number_from_file("number.txt")
 # iterate through the numbers from last_number + 1 to 9999
 for number in range(last_number + 1, 10000):
     # flash the LED for 1 second before starting a new number and also blink shortly to start input
+    set_led_state(True)
+    time.sleep(1)
+    set_led_state(False)
+    time.sleep(1)
+    set_led_state(True)
+    time.sleep(0.5)
+    set_led_state(False)
+    time.sleep(0.5)
+    # convert the number to a string and pad it with leading zeros
+    number_str = format(number, '04d')
+
+    # iterate through the digits of the number
+    for digit in number_str:
+        # convert the digit to an integer
+        digit_int = int(digit)
+
+        # blink the LED for the appropriate number of times
+        for i in range(digit_int):
+            # turn the LED on
+            set_led_state(True)
+            # wait for 200ms
+            time.sleep(0.1)
+            # turn the LED off
+            set_led_state(False)
+            # wait for 200ms
+            time.sleep(0.1)
+
+        # pause for 1 second between digits
+        time.sleep(4.0)
+        
+    # output the number to the console
+    print(number_str)
+    # write the number to the file "number.txt" with the current date and time
+    write_number_to_file("number.txt", number_str, datetime.datetime.now())
+
+# clean up the GPIO resources
+GPIO.cleanup()
